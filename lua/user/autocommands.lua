ac89d20ff8561c24bb02b66b892ba01367e60752
Brewin Vaz <brewin.vaz@gmail.com>
2022-02-26 00:56:32 -0500

    Turn own relative number in insertion mode and hybrid numbers in normal mode.

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -28,6 +28,12 @@ vim.cmd [[
     autocmd!
     autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
   augroup end
+
+  augroup numbertoggle
+    autocmd!
+    autocmd BufEnter,FocusGained,InsertLeave,WinEnter * if &nu && mode() != "i" | set rnu   | endif
+    autocmd BufLeave,FocusLost,InsertEnter,WinLeave   * if &nu                  | set nornu | endif
+  augroup END
 ]]
 
 -- Autoformat
