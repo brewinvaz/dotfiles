3bcd130329831f544b83db18da2c8fe7ce6609ca
Brewin Vaz <brewin@sideqik.com>
2022-07-24 16:27:54 -0400

    Attach aerial to metals server.

--- lua/user/lsp/settings/metals.lua
+++ lua/user/lsp/settings/metals.lua
@@ -9,3 +9,8 @@ metals_config.settings = {
   showImplicitArguments = true,
   -- excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
 }
+
+-- Attach aerial independent of other lsp servers
+metals_config.on_attach = function(client, bufnr)
+  require("aerial").on_attach(client, bufnr)
+end
