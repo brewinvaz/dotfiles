78a3e9adb7245ea4ec6ee07b5c45911488d0858b
Brewin Vaz <brewin@sideqik.com>
2022-08-04 01:47:33 -0400

    Make scala lsp keymaps work (hacky); other misc config updates.

--- lua/user/lsp/settings/solargraph.lua
+++ lua/user/lsp/settings/solargraph.lua
@@ -1,4 +1,12 @@
+local status_ok, lsp_config = pcall(require, "lspconfig")
+if not status_ok then
+  return
+end
+
 return {
   filetypes = { "ruby" },
-  -- root_dir = root_pattern("Gemfile", ".git"),
+  flags = {
+    debounce_text_changes = 150,
+  },
+  root_dir = lsp_config.util.root_pattern("Gemfile", ".git"),
 }
