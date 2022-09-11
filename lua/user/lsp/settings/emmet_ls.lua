c1f75b833a65657079c250c286e7f29cec88f951
Brewin Vaz <brewin@sideqik.com>
2022-09-10 21:55:41 -0400

    Add handlebars to emmet supported types.

--- lua/user/lsp/settings/emmet_ls.lua
+++ lua/user/lsp/settings/emmet_ls.lua
@@ -1,3 +1,3 @@
 return {
-	filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "erb", "eruby" },
+	filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "erb", "eruby", "handlebars" },
 }
