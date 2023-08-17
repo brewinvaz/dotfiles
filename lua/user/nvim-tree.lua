58c4a6a62ae06ce760f391d85aa611358a1549db
Brewin Vaz <brewinvaz@users.noreply.github.com>
2023-08-17 02:26:59 -0400

    Update nvim tree and corresponding config.

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -5,12 +5,24 @@ if not status_ok then
   return
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-  return
-end
+local function on_attach(bufnr)
+  local api = require "nvim-tree.api"
 
-local tree_cb = nvim_tree_config.nvim_tree_callback
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent, opts('Up'))
+  vim.keymap.set('n', '?',     api.tree.toggle_help,           opts('Help'))
+  vim.keymap.set('n', 'l',     api.node.open.edit,             opts('Open'))
+  vim.keymap.set('n', 'h',     api.node.navigate.parent_close, opts('Close Directory'))
+  vim.keymap.set('n', '<C-v>', api.node.open.vertical,         opts('Open: Vertical Split'))
+  vim.keymap.set('n', '<C-x>', api.node.open.horizontal,       opts('Open: Horizontal Split'))
+end
 
 -- following options are being migrated to the setup function incrementally
 -- please check https://github.com/kyazdani42/nvim-tree.lua/blob/master/README.md
@@ -25,6 +37,7 @@ vim.g.show_icons = {
 }
 
 nvim_tree.setup({
+  on_attach = on_attach,
   disable_netrw = true,
   hijack_netrw = true,
   open_on_tab = false,
@@ -65,14 +78,6 @@ nvim_tree.setup({
     width = 35,
     hide_root_folder = false,
     side = "left",
-    mappings = {
-      custom_only = false,
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
-        { key = "h", cb = tree_cb("close_node") },
-        { key = "v", cb = tree_cb("vsplit") },
-      },
-    },
     number = true,
     relativenumber = true,
   },
