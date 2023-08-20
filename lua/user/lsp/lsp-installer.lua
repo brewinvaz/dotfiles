0b011b4cb166b7477978d3d6117cf079af5c6dc1
Brewin Vaz <brewinvaz@users.noreply.github.com>
2023-08-20 01:23:09 -0400

    Change lsp config.

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -1,47 +1,42 @@
-local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
+local status_ok = pcall(require, "nvim-lsp-installer")
 if not status_ok then
 	return
 end
 
--- Register a handler that will be called for all installed servers.
--- Alternatively, you may also register handlers on specific server instances instead (see example below).
-lsp_installer.on_server_ready(function(server)
-	local opts = {
-		on_attach = require("user.lsp.handlers").on_attach,
-		capabilities = require("user.lsp.handlers").capabilities,
-	}
+local status_lspconfig_ok, lspconfig = pcall(require, "lspconfig")
+if not status_lspconfig_ok then
+	return
+end
 
-	if server.name == "jsonls" then
-		local jsonls_opts = require("user.lsp.settings.jsonls")
-		opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
-	end
+local opts = {
+  on_attach = require("user.lsp.handlers").on_attach,
+  capabilities = require("user.lsp.handlers").capabilities,
+}
 
-	if server.name == "sumneko_lua" then
-		local sumneko_opts = require("user.lsp.settings.sumneko_lua")
-		opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-	end
+-- Explicity register each lsp server
+-- Install each lsp servr using `npm i -g <lsp server package name`
 
-	if server.name == "pyright" then
-		local pyright_opts = require("user.lsp.settings.pyright")
-		opts = vim.tbl_deep_extend("force", pyright_opts, opts)
-	end
+local emmet_ls_opts = require("user.lsp.settings.emmet_ls")
+emmet_ls_opts = vim.tbl_deep_extend("force", emmet_ls_opts, opts)
+lspconfig.emmet_ls.setup(emmet_ls_opts)
 
-	if server.name == "emmet_ls" then
-		local emmet_ls_opts = require("user.lsp.settings.emmet_ls")
-		opts = vim.tbl_deep_extend("force", emmet_ls_opts, opts)
-	end
+local html_opts = require("user.lsp.settings.html")
+html_opts = vim.tbl_deep_extend("force", html_opts, opts)
+lspconfig.html.setup(html_opts)
 
-	if server.name == "html" then
-		local html_opts = require("user.lsp.settings.html")
-		opts = vim.tbl_deep_extend("force", html_opts, opts)
-	end
+local jsonls_opts = require("user.lsp.settings.jsonls")
+jsonls_opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+lspconfig.jsonls.setup(jsonls_opts)
 
-	if server.name == "solargraph" then
-		local solargraph_opts = require("user.lsp.settings.solargraph")
-		opts = vim.tbl_deep_extend("force", solargraph_opts, opts)
-	end
+local pyright_opts = require("user.lsp.settings.pyright")
+pyright_opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+lspconfig.pyright.setup(pyright_opts)
 
-	-- This setup() function is exactly the same as lspconfig's setup function.
-	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-	server:setup(opts)
-end)
+lspconfig.cssls.setup(opts)
+lspconfig.cssmodules_ls.setup(opts)
+lspconfig.eslint.setup(opts)
+lspconfig.graphql.setup(opts)
+lspconfig.lua_ls.setup(opts)
+lspconfig.tailwindcss.setup(opts)
+lspconfig.tsserver.setup(opts)
+
